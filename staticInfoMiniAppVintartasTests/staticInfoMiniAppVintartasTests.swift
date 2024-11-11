541982bfb9a5395e2847d99b727b86863f1b7a0d
Nick VIntartas <nv2009123@gmail.com>
2024-11-11 11:28:54 -0600

    Initial Commit

---
+++ staticInfoMiniAppVintartasTests/staticInfoMiniAppVintartasTests.swift
@@ -0,0 +1,17 @@
+//
+//  staticInfoMiniAppVintartasTests.swift
+//  staticInfoMiniAppVintartasTests
+//
+//  Created by NICHOLAS VINTARTAS on 11/11/24.
+//
+
+import Testing
+@testable import staticInfoMiniAppVintartas
+
+struct staticInfoMiniAppVintartasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
